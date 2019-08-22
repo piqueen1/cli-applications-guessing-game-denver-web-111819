d6316e06870a43d90fd31e9a867d92e418daa9c7
Finn Thye <piqueen@gmail.com>
2019-08-22 00:32:07 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -2,15 +2,18 @@ require "pry"
 
 def prompt
   puts "Please enter a number between 1 and 6"
+  binding.pry
 end
 
 def run_guessing_game
+  binding.pry
   my_num = rand(6).to_i + 1
   prompt
   user_num = gets.strip
+  binding.pry
   
   message = if user_num == my_num
-    puts /You guessed the correct number!/
+    puts "You guessed the correct number!"
   elsif user_num == "exit"
     puts "Goodbye!"
   else
